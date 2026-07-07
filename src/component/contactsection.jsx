e691b480c5c7da3a6a10768d4d4e693e19608978
Sneha-datla <datla.sneha1@gmail.com>
2026-07-07 12:37:12 +0530

    changes made

--- src/component/contactsection.jsx
+++ src/component/contactsection.jsx
@@ -109,11 +109,15 @@ export function ContactSection() {
         }
 
         /* INFO BOX */
-      .info-box {
-  width: 100%;
-  margin: 0;
-  padding: 24px;
-  box-sizing: border-box;
+     .info-box {
+  background:#fff5ec;
+  width:100%;
+  padding:24px;
+  border-radius:22px;
+  display:flex;
+  flex-direction:column;
+  gap:28px;
+  box-sizing:border-box;
 }
 
         /* TABLET */
